7104e548bcf8815556ba0bbec340d44b12ec2edd
Yeats Goodwin <yeatsg@gmail.com>
2022-03-21 13:28:58 -0400

    new link page in development

---
+++ final-project-frontend/src/components/createTree.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CreateTree = () => {
+  return (
+    <div>
+      <h2>Please select below</h2>
+      <div>
+        <h3>Find Top 5 Artists</h3>
+        <p>Spotify will find </p>
+      </div>
+      <div>
+        <h3>Search by Track Name</h3>
+        <p>Go through the Track Listing</p>
+      </div>
+      <div>
+        <h3>Search by User Playlist</h3>
+        <p>Playlist</p>
+      </div>
+    </div>
+  );
+};
+
+export default CreateTree;
